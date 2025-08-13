9dc851c7ac93fd3f4c1b2433a7f04e481fcedc50
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-08-13 16:44:20 +0530

    Sub category updated for notification

--- src/main/java/com/igot/cb/pores/util/Constants.java
+++ src/main/java/com/igot/cb/pores/util/Constants.java
@@ -280,7 +280,7 @@ public class Constants {
     public static final String HEADER_MAP = "headerMap";
     public static final String REQUEST_BODY = "requestBody";
     public static final String SERVICE_CODE = "serviceCode";
-    public static final String PROFANITY_CHECK = "PROFANITY-CHECK";
+    public static final String PROFANITY_CHECK = "PROFANITY_CHECK";
     public static final String LANGUAGE_CODE = "languageCode";
     public static final String REQUEST_DATA = "request_data";
     public static final String RESPONSE_DATA = "response_data";
